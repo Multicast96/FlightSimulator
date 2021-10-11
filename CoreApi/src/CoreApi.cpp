8483dfb5b0266ce9841f936669aa866bfd92f68a
Kamil <kamil.dakus@gmail.com>
2021-10-11 21:04:44 +0200

    Project setup

---
+++ CoreApi/src/CoreApi.cpp
@@ -0,0 +1,13 @@
+﻿// CoreApi.cpp : Defines the entry point for the application.
+//
+
+#include "CoreApi.h"
+#include "../dependencies/cpp-httplib/httplib.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
